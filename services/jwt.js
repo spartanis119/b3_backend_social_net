32ccd3cf9b75c4c87d6811714bdb77ee8aa6cdcc
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2024-10-19 16:43:18 -0500

    login and generate token OK

---
+++ services/jwt.js
@@ -0,0 +1,28 @@
+import jwt from 'jwt-simple';
+import moment from 'moment';
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+// Clave secreta
+const secret = process.env.SECRET_KEY;
+
+// Método para generar tokens
+// Unix: segundos transcurridos desde el 1 de enero de 1970 hasta hoy.
+const createToken = (user) => {
+    const payload = {
+        userId: user._id,
+        role: user.role,
+        iat: moment().unix(), // fecha de emisión
+        exp: moment().add(7, 'days').unix() // fecha de expiración
+    }
+
+    // Devolver el jwt token codificado
+    return jwt.encode(payload, secret);
+};
+
+export {
+    secret,
+    createToken
+};
